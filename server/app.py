66f1b52a921c53e6bc221873f892a652e0455c93
Student2297 <106355731+jiaqicheng1104@users.noreply.github.com>
2023-05-21 06:39:21 -0400

    db connected

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
 from flask import Flask 
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
+client = MongoClient("mongodb+srv://jiaqicheng1104:ugDWOD0Oqvc9wVW5@dta.utdqct1.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["DTA"]
+user_collection = db["user"]
 
 @app.route('/api', methods=['GET'])
 def index():
