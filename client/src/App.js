dbb0ef57f4eeb5598b4783bc6ca57056a41ff0de
siliang0426 <SIL71@pitt.edu>
2023-05-22 19:05:23 -0400

    IntroPage update
    Testing with database disabled

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
+import React from "react";
 import { useEffect, useState } from "react";
+import IntroPage from './AppComponent/AppComponent.js';
 
 function App() {
-  const [obj, setObj] = useState({});
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api") // Take the redirected port from docker composed
-      .then((res) => res.json())
-      .then((data) => setObj(data));
-  }, []);
-
-  if (obj === {}) {
-    return <div>Loading</div>;
-  }
   return (
     <div>
-      <h1>{obj.channel}</h1>
-      <h2>{obj.tutorial}</h2>
+      <IntroPage />
     </div>
   );
 }
---
+++ client/src/AppComponent/AppComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+import './AppComponent.css';
+
+function IntroPage(){
+    return(
+    <div className="AppComponent">
+        <h1>Daily Task Assistant</h1>
+        <h2>Plan, Executive and Evaluate</h2>
+        <h3>
+            By:{<br />}Jiaqi Cheng{<br />}Zuxiang Wang{<br />}Siliang Lihuang
+        </h3>
+        <button id="signup" >Get Started</button>
+        <button id="signin">Sign In</button>
+    </div>
+    );
+}
+
+export default Page;
