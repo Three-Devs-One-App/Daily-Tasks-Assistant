43fa41ccb7dde71c2a1a14e833994bd1c9354f04
zuxiangw <zuxiangwang2002@gmail.com>
2023-06-02 22:46:10 -0400

    added utilsjs and components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import IntroPage from './AppComponent/AppComponent.js';
+import IntroPage from "./AppComponent/AppComponent.js";
 
 function App() {
   return (
