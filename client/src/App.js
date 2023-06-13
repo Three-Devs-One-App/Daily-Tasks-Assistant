c94721d8476efca45e2539dbb0f4ec18a67d09c6
siliang0426 <SIL71@pitt.edu>
2023-06-13 07:05:25 -0400

    Login session functioning

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,11 @@ const App = () => {
 
   useEffect(() => {
     fetch('http://localhost:8080/check-login')
-      .then(response => {
-        setLoggedIn(response.data.loggedIn);
+      .then(response => response.json())
+      .then(data => {
+        setLoggedIn(data.loggedIn);
         console.log("inside useeffect change")
-        console.log(loggedIn)
+        console.log(loggedIn) // <-- This will not reflect the updated value immediately
       })
       .catch(error => {
         console.error('Error checking login state', error);
@@ -27,7 +28,7 @@ const App = () => {
   const handleSessionChange= (value) => {
     setLoggedIn(value);
     console.log("inside handle change")
-    console.log(loggedIn)
+    console.log(value)
   };
 
   const handleLogout = () => {
@@ -40,104 +41,17 @@ const App = () => {
       });
   };
 
+  console.log(Page)
   console.log(loggedIn)
-
   return (
     <div className="App">
       {!loggedIn && Page === "" && <IntroPage setPage={setPage} />}
       {!loggedIn &&Page === "SignUp" && <SignUp setPage={setPage} />}
       {!loggedIn &&Page === "Login" && <Login setPage={setPage} sessionStatus={handleSessionChange} />}
       {!loggedIn &&Page === "Forget" && <ForgetPasswordPage setPage={setPage} />}
-      {loggedIn &&Page === "Profile" && <Profile setPage={setPage} sessionStatus={handleLogout}/>}
+      {(loggedIn ||Page === "Profile") && <Profile setPage={setPage} sessionStatus={handleLogout}/>}
     </div>
   );
 };
-
-/*
-function App() {
-  const [state, setState] = useState("SignUp"); //debug use for default setting signup page
-
-  const [user, setuser] = useState({});
-
-  const handleClickSignUp = () => {
-    setState("SignUp");
-  };
-  const SignInClicked = () => {
-    setState("Login");
-  };
-  const handleClickAlreadySignUp = () => {
-    setState("Login");
-  };
-  const handleClickForget = () => {
-    setState("Forget");
-  };
-  const handleUserSignUp = (inputData) => {
-    setuser(inputData);
-    setState("profile");
-
-    console.log("success");
-
-    fetch("http://localhost:8080/user", {
-      method: "POST", //communicate to backend
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inputData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-  const handleClickLogin = (UserData) => {
-    setuser(UserData);
-    setState("profile");
-
-    fetch("http://localhost:8080/Profile", {
-      method: "POST", //communicate to backend
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(UserData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
-  return (
-    <div className="App">
-      {state === "Intro" && (
-        <IntroPage
-          signupClicked={handleClickSignUp}
-          SignInClicked={SignInClicked}
-          forgetClicked={handleClickForget}
-        />
-      )}
-      {state === "SignUp" && (
-        <Signup
-          alreadysignclick={handleClickAlreadySignUp}
-          submitSignclick={handleUserSignUp}
-        />
-      )}
-      {state === "Login" && (
-        <Login
-          ClickLogin={handleClickLogin}
-          SignupClicked={handleClickSignUp}
-        />
-      )}
-      {state === "profile" && <Profile user={user} />}
-      {state === "Forget" && <ForgetPasswordPage />}
-    </div>
-  ); //the al-signup is temperaily change to intro page now
-}
-*/
 
 export default App;
